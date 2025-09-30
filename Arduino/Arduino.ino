2abce849b4eb3ae866303e8053aa7f3c12edd059
Eftz <fetuszaza@gmail.com>
2025-09-30 13:45:25 +0700

    Finished and Rearrange code for Wired on Project

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,10 +1,8 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <avr/wdt.h>  // Watchdog
 #include <Servo.h>    // Servo Radar
 
 #define SLAVE_ADDR 0x08
-// LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Radar
 #define TRIG1 2
@@ -98,12 +96,12 @@ void GetRequest() {
 // Radar
 long radar(int trig, int echo) {
   digitalWrite(trig, LOW);
-  delayMicroseconds(5);
+  delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  long duration = pulseIn(echo, HIGH, 40000);  // timeout 40ms
+  long duration = pulseIn(echo, HIGH, 40000); 
   if (duration == 0) return -1;                // no echo
   int distance = duration * 0.034 / 2;
   return distance;
@@ -118,8 +116,8 @@ void setup() {
   Wire.onReceive(receiveEvent);
   Wire.onRequest(GetRequest);
 
-  // Interrupt to ESP32
-  pinMode(SendISRtoESP32, OUTPUT);  // ขา output ไป ESP32
+  // Interrupt output to ESP32
+  pinMode(SendISRtoESP32, OUTPUT);  // 
 
   // Setup radar
   pinMode(ECHO1, INPUT);
@@ -153,33 +151,31 @@ void setup() {
   // Radar Servo
   RadarServo.attach(ServoDrift, 500, 2500);
   delay(50);
-  RadarServo.write(0);
-  // เปิด watchdog 8 วินาที
+
+  // Set watchdog 8 s
   wdt_enable(WDTO_8S);
 }
 
 void loop() {
-  // Move servo foreach 1 degree
-  // อ่าน Radar ทุก sensor
+  // active sensor foreach (faster + decrease corrision)
   dt1 = radar(TRIG1, ECHO1);
   wdt_reset();
-  delay(30);
   dt2 = radar(TRIG2, ECHO2);
   wdt_reset();
-  delay(30);
   dt3 = radar(TRIG3, ECHO3);
   wdt_reset();
-  delay(30);
   dt4 = radar(TRIG4, ECHO4);
   wdt_reset();
-  delay(30);
+  delay(50);
 
-  // ขยับ Servo
+  // move Servo
   RadarServo.write(angle);
-  delay(10);
 
-  // ตรวจจับวัตถุ
-  if ((dt1 != -1 && dt1 <= 10) || (dt2 != -1 && dt2 <= 10) || (dt3 != -1 && dt3 <= 10) || (dt4 != -1 && dt4 <= 10)) {
+  // Radar detect Alarm , Interrupt to ESP32
+  if ((dt1 != -1 && dt1 <= 10) || 
+      (dt2 != -1 && dt2 <= 10) || 
+      (dt3 != -1 && dt3 <= 10) || 
+      (dt4 != -1 && dt4 <= 10)) {
     tone(BuzzerAlarm, 1000, 200);
     if (!pulseActive) {
       digitalWrite(SendISRtoESP32, HIGH);
@@ -193,21 +189,20 @@ void loop() {
     pulseActive = false;
   }
 
-  // เปลี่ยนค่า angle
+  // move angle 1 degree per loop
   angle += stepDir;
   if (angle <= 0) {
     angle = 0;
     stepDir = +1;
-  }  // ถึง 0° → เปลี่ยนทิศ
+  }
   if (angle >= 180) {
     angle = 180;
     stepDir = -1;
-  }  // ถึง 180° → เปลี่ยนทิศ
+  }
 
-  // 🔹 เช็คคำสั่งจาก ESP32
+  // Order check from ESP32
   if (newData) {
-    newData = false;  // Clear flag
-
+    newData = false;
     if (Lock1 == 1) {
       LockServo.write(70);
       tone(BuzzerAlarm, 1000, 200);
@@ -217,6 +212,13 @@ void loop() {
     }
   }
 
-  wdt_reset();  // กัน timeout
+  // Serial Radar Monitor
+  Serial.print(angle);
+  Serial.print(dt1);
+  Serial.print(dt2);
+  Serial.print(dt3);
+  Serial.println(dt4);
+
+  wdt_reset();  // กัน watchdog reset
 }
 
